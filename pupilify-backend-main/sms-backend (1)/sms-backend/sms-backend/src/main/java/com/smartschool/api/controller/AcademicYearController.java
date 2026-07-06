8f0b107118211b8627f5ae8eb0a3c3465e58715a
nikunj9201 <nikunjpatidar475@gmail.com>
2026-07-06 19:49:05 +0530

    Updated develop branch

--- pupilify-backend-main/sms-backend (1)/sms-backend/sms-backend/src/main/java/com/smartschool/api/controller/AcademicYearController.java
+++ pupilify-backend-main/sms-backend (1)/sms-backend/sms-backend/src/main/java/com/smartschool/api/controller/AcademicYearController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/academic-years")
+@RequestMapping("/api/academic-year")
 @CrossOrigin("*")
 public class AcademicYearController {
 
