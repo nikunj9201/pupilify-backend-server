3014be2685b4af05716df064acf339910e104d0a
nikunj9201 <nikunjpatidar475@gmail.com>
2026-07-07 15:57:29 +0530

    ICard feature updated

--- pupilify-backend-main/sms-backend (1)/sms-backend/sms-backend/src/main/java/com/smartschool/api/controller/AcademicYearController.java
+++ pupilify-backend-main/sms-backend (1)/sms-backend/sms-backend/src/main/java/com/smartschool/api/controller/AcademicYearController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/academic-years")
+@RequestMapping("/api/superadmin/academic-year")
 @CrossOrigin("*")
 public class AcademicYearController {
 
